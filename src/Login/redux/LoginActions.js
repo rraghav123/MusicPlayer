b2075480e595956af86a32a0966ff5aa18051ddf
Smoak <raghav.mahajan@evivehealth.com>
2019-06-07 10:42:32 +0530

    removed login reducer

--- src/Login/redux/LoginActions.js
+++
@@ -1,7 +0,0 @@
-import ActionTypes from "./LoginActionTypes";
-
-const LoginActions = {
-    setLoadingStatus: payload => ({ type: ActionTypes.SET_LOADING_STATUS, payload })
-};
-
-export default LoginActions;
--- src/Reducer.js
+++ src/Reducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
-import LoginReducer from "./Login/redux/LoginReducer";
 import HomeReducer from "./Home/redux/HomeReducer";
 
 export default combineReducers({
-  login: LoginReducer,
   home: HomeReducer
 });
